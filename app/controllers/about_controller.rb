4f3f0643afdbfffb4cc7b9d171b54f9f1e38a973
Shane Becker <veganstraightedge@gmail.com>
2008-11-06 16:56:35 -0800

    people table styles

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,6 @@
 class AboutController < ApplicationController
   def index
-    @dudes    = Dude.all.sort_by{|d| d.name}
+    @dudes    = Dude.all.sort_by{|d| d.projects.length}.reverse
     @projects = Project.all.sort_by{|d| d.name}
   end
 end
