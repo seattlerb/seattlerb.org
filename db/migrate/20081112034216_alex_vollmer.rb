7f3517c6ea285120cfa6d57e849884478fa4a848
Shaners Becker <veganstraightedge@gmail.com>
2008-11-11 20:32:07 -0800

    clean out dudes

--- db/migrate/20081112034216_alex_vollmer.rb
+++ db/migrate/20081112034216_alex_vollmer.rb
@@ -1,16 +1,15 @@
 class AlexVollmer < ActiveRecord::Migration
   def self.up
-    pci4r = Project.create!(
-              :name => "pci4r",
-              :description => "Ruby port of Programming Collective Intelligence",
-              :url => "http://github.com/alexvollmer/pci4r"
-            )
+    Project.create!(
+      :name => "pci4r",
+      :description => "Ruby port of Programming Collective Intelligence",
+      :url => "http://github.com/alexvollmer/pci4r"
+    )
 
-    alex = Dude.find_by_name("Alex Vollmer").update_attributes!(
-              :website => "http://livollmers.net",
-              :email   => "alex.vollmer@gmail.com",
-              :bio     => "Alex is a frustrated heavy-metal guitarist who once beat Jack Butler in a duel with a Toys-R-Us guitar and a popsicle stick for a pick. But since rad metal guitarists are currently out of fashion, he moonlights as a computer geek until that day when metal returns to take its rightful place."
-            )
+    Dude.find_by_name("Alex Vollmer").update_attributes!(
+      :website => "http://livollmers.net",
+      :email   => "alex.vollmer@gmail.com"
+    )
 
     Affiliation.create!(:dude => Dude.find_by_name("Alex Vollmer"), :project => Project.find_by_name("pci4r"))
   end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081112034216) do
+ActiveRecord::Schema.define(:version => 20081112042602) do
 
   create_table "affiliations", :force => true do |t|
     t.integer  "dude_id"
