24a41e667afb46136750f9a5a3b7293c2445f039
Phil Hagelberg <technomancy@gmail.com>
2009-03-31 09:52:35 -0700

    Make affiliations destroyed when their dude or project is destroyed.

--- app/models/dude.rb
+++ app/models/dude.rb
@@ -1,5 +1,5 @@
 class Dude < ActiveRecord::Base
   validates_presence_of :name
-  has_many :affiliations
+  has_many :affiliations, :dependent => :destroy
   has_many :projects, :through => :affiliations
 end
