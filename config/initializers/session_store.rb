0ea97c52c7e609e981fd7bce90547c45be96c4c0
John Barnette <jbarnette@gmail.com>
2009-07-23 16:11:41 -0700

    Remove boilerplate comment cruft.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+ActionController::Base.session = {
+  :key         => '_roo_session',
+  :secret      => '377c1be6ea072d4255033dfcf8a8326b58dbecfe839fd9723f150dd76630e15341141972417d8ea6cfd031b0be5ed31b35e27e483a4cff88becba9cf476892e1'
+}
