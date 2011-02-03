5cdc9c53df3e35bbb0bf78e98cc5c7c33acaff7f
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-03 14:13:26 -0800

    remove dead code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "about"
-  # map.resources :projects, :dudes
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
