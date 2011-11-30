eaa48e38fa5288f0604c16c13035de4a349aeb3c
Daniel Dosen <ddosen@gmail.com>
2011-11-30 15:19:25 -0800

    Revert "remove dead code"
    This reverts commit 5cdc9c53df3e35bbb0bf78e98cc5c7c33acaff7f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "about"
+  # map.resources :projects, :dudes
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
