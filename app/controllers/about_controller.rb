7614b846c04fe23862dd2c59cebad36bf8a57c5d
Bookis Smuin <vegan.bookis@gmail.com>
2012-06-13 17:10:22 -0400

    add people, projects, and join us page from github pages site

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,4 +3,15 @@ class AboutController < ApplicationController
     @dudes    = Dude.all.sort_by{|d| d.projects.length}.reverse
     @projects = Project.all.sort_by{|d| d.name.downcase}
   end
+  
+  def people
+  end
+  
+  def projects
+    
+  end
+  
+  def join_us
+    
+  end
 end
