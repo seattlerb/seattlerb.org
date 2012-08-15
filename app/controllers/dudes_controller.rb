3522456f4de813ac7ee001c32c6bf41c997f2b10
Bookis <vegan.bookis@gmail.com>
2012-08-14 20:30:52 -0700

    add people destroy

--- app/controllers/dudes_controller.rb
+++ app/controllers/dudes_controller.rb
@@ -16,5 +16,11 @@ class DudesController < ApplicationController
       render :edit
     end
   end
+  
+  def destroy
+    @dude = Dude.find(params[:id])
+    @dude.destroy
+    redirect_to dudes_path
+  end
 
 end
