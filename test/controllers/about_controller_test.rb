8dc8b1083646565b7c4f73cb652d7fada6342332
Aja Hammerly <aja.hammerly+github@gmail.com>
2012-12-18 19:42:16 -0800

    -Added tests for about controller

---
+++ test/controllers/about_controller_test.rb
@@ -0,0 +1,30 @@
+require "minitest_helper"
+
+class AboutControllerTest < MiniTest::Rails::ActionController::TestCase
+  def test_index
+    get :index
+    assert_response :success
+    assert_equal("Home", assigns(:title))
+  end
+
+  def test_people
+    people = (1..3).map{ |x| Dude.create!(:name => x, :ruby_gems_id => "#{x}_id")}
+
+    get :people
+    assert_response :success
+    assert_equal people.sort, assigns(:people).sort
+  end
+
+  def test_projects
+    projects = (1..3).map { |x| Project.create!(:name => x)}
+
+    get :projects
+    assert_response :success
+    assert_equal projects.sort, assigns(:projects).sort
+  end
+
+  def test_join_us
+    get :join_us
+    assert_response :success
+  end
+end
