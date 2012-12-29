ba29ae2a05e1dbaa729a1bd8a910b1339b893d53
Ryan Davis <ryand-ruby@zenspider.com>
2012-12-29 05:13:30 -0800

    minor cleanup

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,8 +1,5 @@
 class Talk < ActiveRecord::Base
-  TALK_KINDS = ["beginner",
-                "intermediate",
-                "advanced",
-                "lightning"]
+  TALK_KINDS = %w(beginner intermediate advanced lightning)
 
   validates :kind, :inclusion => { :in => TALK_KINDS,
     :message => "%{value} is not a valid talk kind" }
