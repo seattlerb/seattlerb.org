89cdc43486d3465d50a26ab3cb6f2431f5ae36c6
Ryan Davis <ryand-ruby@zenspider.com>
2012-12-31 17:55:35 -0800

    Cleaned up people page links. Ugh.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,33 @@
 module ApplicationHelper
-  
   def twitter_image(twitter_handle)
     "https://api.twitter.com/1/users/profile_image?screen_name=#{twitter_handle}&size=bigger"
   end
-  
-  def twitter_link(twitter)
-    return if twitter.blank?
-    "<li><i class='icon-leaf'></i>" +
-    link_to("@#{twitter}", "http://twitter.com/#{twitter}") +
-    "</li>"
+
+  def icon_link icon, text, uri, uri_prefix=nil
+    return if uri.blank?
+
+    "<li><i class='#{icon}'></i>#{link_to text, "#{uri_prefix}#{uri}"}</li>"
+  end
+
+  def rubygems_link person
+    icon_link("icon-heart", "Ruby Gems",
+              person.ruby_gems_id, "http://rubygems.org/profiles/")
+  end
+
+  def github_link person
+    icon_link "icon-glass", "Github", person.github
+  end
+
+  def website_link person
+    icon_link "icon-user", "Website", person.website
+  end
+
+  def email_link person
+    icon_link "icon-envelope", "Email", person.email, "mailto:"
+  end
+
+  def twitter_link person
+    v = person.twitter
+    icon_link "icon-leaf", "@#{v}", v, "http://twitter.com/"
   end
 end
