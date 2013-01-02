0bca971abd683e96ed4b98e446d6e46ee0f41e32
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-01-01 19:23:10 -0800

    Removed puts in controller code

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -1,7 +1,6 @@
 class NewsletterController < ApplicationController
   def subscribe
     response = ZenspiderSubscriber.subscribe!(params[:subscribe])
-    puts response.inspect
     redirect_to join_us_path
   end
 end
