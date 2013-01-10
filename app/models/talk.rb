1ffa8175d93b15bc4739e6378aeb7d0d3bcd70db
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-01-09 20:12:30 -0800

    Talk sort order - ping!

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,7 +1,11 @@
 class Talk < ActiveRecord::Base
   TALK_KINDS = %w(beginner intermediate advanced lightning)
 
-  default_scope :order => "kind, title"
+  default_scope :order => "case kind when 'beginner'     then 1
+                                     when 'intermediate' then 2
+                                     when 'advanced'     then 3
+                                     when 'lightning'    then 4
+                           end, title"
 
   validates(:kind,
             :inclusion => {
