495c338556e2e26f930eb7b45a825afbfc4918c8
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-11 23:51:00 -0700

    front end blah

--- app/assets/javascripts/talks.js
+++ app/assets/javascripts/talks.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 
-  $('.tabs a').on('click', function(e){
+  $('#tabs a').on('click', function(e){
     e.preventDefault();
     console.log('HELLO')
     var newcontent = $(this).attr('href');
     var tab = $(this).parent();
 
-    $('.tabs a').removeClass('sel');
-    $('.tabs a').each(function(){
-      $(this).parent().css('background-color', '#a5c7cf')
+    $('#tabs a').removeClass('sel');
+    $('#tabs a').each(function(){
+      $(this).parent().css('background-color', '#63BFD8')
     });
     $(this).addClass('sel');
-    tab.css('background-color', 'papayawhip');
+    tab.css('background-color', '#A9D0F5');
 
     $('#content section').each(function(){
       if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
