69fd6199d82210a0081399d89520867665c8204a
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-11 23:29:50 -0700

    papayawhip?

--- app/assets/javascripts/talks.js
+++ app/assets/javascripts/talks.js
@@ -4,9 +4,14 @@ $(document).ready(function(){
     e.preventDefault();
     console.log('HELLO')
     var newcontent = $(this).attr('href');
+    var tab = $(this).parent();
 
     $('.tabs a').removeClass('sel');
+    $('.tabs a').each(function(){
+      $(this).parent().css('background-color', '#a5c7cf')
+    });
     $(this).addClass('sel');
+    tab.css('background-color', 'papayawhip');
 
     $('#content section').each(function(){
       if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
