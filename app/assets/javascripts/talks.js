364bd0d82d04d15988bfc56bc68a54c9af0c4f21
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-12 20:11:44 -0700

    toggle tabs for talks

---
+++ app/assets/javascripts/talks.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $('#tabs a').on('click', function(e){
+    e.preventDefault();
+    console.log('HELLO')
+    var newcontent = $(this).attr('href');
+    var tab = $(this).parent();
+
+    $('#tabs a').removeClass('sel');
+    $('#tabs a').each(function(){
+      $(this).parent().css('background-color', '#C3D9FF')
+    });
+    $(this).addClass('sel');
+    tab.css('background-color', '#A9D0F5');
+
+    $('#content section').each(function(){
+      if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
+    });
+
+    $(newcontent).removeClass('hidden');
+  });
+
+});
