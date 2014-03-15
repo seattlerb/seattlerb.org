06370cf92062d6116b84f4d23e197c6377dbad00
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-14 19:21:52 -0700

    cleanup

--- app/assets/javascripts/talks.js
+++ app/assets/javascripts/talks.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
   $('#tabs div').on('click', function(e){
     e.preventDefault();
     var activecontent = $(this).find('a').attr('href');
@@ -7,14 +6,11 @@ $(document).ready(function(){
     $('#tabs div').each(function(){
       if($(this).hasClass('active-tab')) { $(this).removeClass('active-tab'); }
     });
-
     $(this).addClass('active-tab');
 
     $('#content section').each(function(){
       if(!$(this).hasClass('hide')) { $(this).addClass('hide'); }
     });
-
     $(activecontent).removeClass('hide');
   });
-
 });
