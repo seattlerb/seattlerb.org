3b79cee2e0152cdfd9f35c445095a66a922c536c
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-14 19:19:54 -0700

    refactor js and css

--- app/assets/javascripts/talks.js
+++ app/assets/javascripts/talks.js
@@ -3,15 +3,12 @@ $(document).ready(function(){
   $('#tabs div').on('click', function(e){
     e.preventDefault();
     var activecontent = $(this).find('a').attr('href');
-    var tab = $(this).parent();
 
-    // $('#tabs a').removeClass('sel');
-    // $('#tabs a').each(function(){
-    //   $(this).parent().css('background-color', '#C3D9FF')
-    // });
-    // $(this).addClass('sel');
+    $('#tabs div').each(function(){
+      if($(this).hasClass('active-tab')) { $(this).removeClass('active-tab'); }
+    });
 
-    // tab.addClass('active');
+    $(this).addClass('active-tab');
 
     $('#content section').each(function(){
       if(!$(this).hasClass('hide')) { $(this).addClass('hide'); }
