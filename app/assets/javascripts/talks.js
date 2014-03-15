26ddbf36d26f83af8833e676eeaac32ee50d876d
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-14 18:57:08 -0700

    refactor js

--- app/assets/javascripts/talks.js
+++ app/assets/javascripts/talks.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 
-  $('#tabs a').on('click', function(e){
+  $('#tabs div').on('click', function(e){
     e.preventDefault();
-    console.log('HELLO')
-    var newcontent = $(this).attr('href');
+    var activecontent = $(this).find('a').attr('href');
     var tab = $(this).parent();
 
-    $('#tabs a').removeClass('sel');
-    $('#tabs a').each(function(){
-      $(this).parent().css('background-color', '#C3D9FF')
-    });
-    $(this).addClass('sel');
-    tab.css('background-color', '#A9D0F5');
+    // $('#tabs a').removeClass('sel');
+    // $('#tabs a').each(function(){
+    //   $(this).parent().css('background-color', '#C3D9FF')
+    // });
+    // $(this).addClass('sel');
+
+    // tab.addClass('active');
 
     $('#content section').each(function(){
-      if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
+      if(!$(this).hasClass('hide')) { $(this).addClass('hide'); }
     });
 
-    $(newcontent).removeClass('hidden');
+    $(activecontent).removeClass('hide');
   });
 
 });
