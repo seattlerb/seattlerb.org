6d4817bdf98ae73ad360df33468b5806094d3a1e
Kyle Rippey <kyle@avvo.com>
2014-03-18 23:12:45 -0700

    Fixing subscription controller that I broke...

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -1,12 +1,13 @@
 class NewsletterController < ApplicationController
   def subscribe
     ZenspiderSubscriber.subscribe!(subscribe_params)
+
     redirect_to join_us_path
   end
 
   private
 
   def subscribe_params
-    params.permit(:person, :subscribe_to_discussion, :subscribe_to_jobs)
+    params.require(:subscribe).permit({:person => :email}, :subscribe_to_discussion, :subscribe_to_jobs)
   end
 end
