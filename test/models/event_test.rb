653e5169a05facba6e4b81bd77982a284d348804
Lillian Ng <lillian.k.ng@gmail.com>
2014-03-26 14:06:29 -0700

    better first_tues_month

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -11,8 +11,20 @@ class EventTest < MiniTest::Rails::ActiveSupport::TestCase
     refute event.valid?
   end
 
-  def test_next
-    event = Event.create!(:date => Date.today)
-    assert_equal Event.next, event
+  def test_first_tues_month
+    assert_equal Event.first_tues_month(Date.new(2014,1,12)), Date.new(2014,1,7)
+    assert_equal Event.first_tues_month(Date.new(2014,2,18)), Date.new(2014,2,4)
+    assert_equal Event.first_tues_month(Date.new(2014,3,9)), Date.new(2014,3,4)
+    assert_equal Event.first_tues_month(Date.new(2014,4,28)), Date.new(2014,4,1)
+    assert_equal Event.first_tues_month(Date.new(2014,5,12)), Date.new(2014,5,6)
+    assert_equal Event.first_tues_month(Date.new(2014,6,5)), Date.new(2014,6,3)
+    assert_equal Event.first_tues_month(Date.new(2014,7,2)), Date.new(2014,7,1)
+    assert_equal Event.first_tues_month(Date.new(2014,8,7)), Date.new(2014,8,5)
+    assert_equal Event.first_tues_month(Date.new(2014,9,1)), Date.new(2014,9,2)
+    assert_equal Event.first_tues_month(Date.new(2014,10,29)), Date.new(2014,10,7)
+    assert_equal Event.first_tues_month(Date.new(2014,11,12)), Date.new(2014,11,4)
+    assert_equal Event.first_tues_month(Date.new(2014,12,24)), Date.new(2014,12,2)
+
+    assert_equal Event.first_tues_month(Date.new(2014,12,4) + 1.month), Date.new(2015,1,6)
   end
 end
