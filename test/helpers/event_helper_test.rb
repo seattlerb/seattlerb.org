60698a51f723e246e722395accf00bf8ad7a7d3f
Lillian Ng <lillian.k.ng@gmail.com>
2014-03-26 02:19:03 -0700

    added tests for event model

--- test/helpers/event_helper_test.rb
+++
@@ -1,9 +0,0 @@
-require "minitest_helper"
-
-class EventHelperTest < MiniTest::Rails::ActionView::TestCase
-
-  def test_sanity
-    flunk "Need real tests"
-  end
-
-end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,18 @@
 require "minitest_helper"
 
 class EventTest < MiniTest::Rails::ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_valid
+    event = Event.new(:date => '2014-03-26')
+    assert event.valid?
+  end
+
+  def test_invalid
+    event = Event.new
+    refute event.valid?
+  end
+
+  def test_next
+    event = Event.create!(:date => Date.today)
+    assert_equal Event.next, event
+  end
 end
