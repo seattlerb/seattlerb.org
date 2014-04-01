590caa0b0ae3a946af64ee71bf2dde9c39023da8
Todd Lynam <TLynam@gmail.com>
2014-04-01 14:26:42 -0700

    Updated test to include 2 opt-in admins and 1 opt-out admin

--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -9,43 +9,32 @@ class AdminMailerTest < MiniTest::Rails::ActionMailer::TestCase
                                    :presenter         => "Barack Obama",
                                    :description       => "Talk about Rails 4 vulnerability fixes.")
     
-    admin_optin    =  User.create!(:email             => "rubyadmin@gmail.com",
+    admin_optin1    =  User.create!(:email             => "rubyadmin1@gmail.com",
                                    :password          => "password123",
                                    :talk_notification => true)
- 
-    mail = AdminMailer.admin_notification(admin_optin.email,talk)
 
+    admin_optin2    =  User.create!(:email             => "rubyadmin2@gmail.com",
+                                   :password          => "password123",
+                                   :talk_notification => true)
+
+    admin_optout   =  User.create!(:email             => "rubyadmin_optout@gmail.com",
+                                   :password          => "password123")
+ 
+     admins = User.find_all_by_talk_notification(true).map(&:email)
+     if !admins.empty? then
+       mail = AdminMailer.admin_notification(admins,talk)
+     end
+
+    assert_match /noreply@seattlerb.org/, mail.from.first
+    assert_match /Seattle.rb Talk Notification/, mail.subject
+    assert mail.to.include?("rubyadmin1@gmail.com")
+    assert mail.to.include?("rubyadmin2@gmail.com")
+    assert !mail.to.include?("rubyadmin_optout@gmail.com")
     assert_match /beginner/, mail.encoded
     assert_match /Rails Security Vulnerabilities/, mail.encoded
     assert_match /railsguru@aol.com/, mail.encoded
     assert_match /Barack Obama/, mail.encoded 
     assert_match /Talk about Rails 4 vulnerability fixes./, mail.encoded
-    assert_match /rubyadmin@gmail.com/, mail.to.last
-    assert_match /noreply@seattlerb.org/, mail.from.first
-    assert_match /Seattle.rb Talk Notification/, mail.subject
-  end
-
-  def test_talk_notification_opt_out
-    talk     =            Talk.new(:kind              => "beginner",
-                                   :title             => "Rails Security Vulnerabilities",
-                                   :email             => "railsguru@aol.com",
-                                   :presenter         => "Barack Obama",
-                                   :description       => "Talk about Rails 4 vulnerability fixes.")
-
-    admin_optout   =  User.create!(:email             => "rubyadmin_optout@gmail.com",
-                                   :password          => "password123")
-
-    admins = User.find_all_by_talk_notification(true).map(&:email)
-    if !admins.empty? then
-      AdminMailer.admin_notification(admins,talk).deliver
-    end    
-
-    admins_all = User.all.map(&:email)
-    admins_talk_true = User.find_all_by_talk_notification(true).map(&:email)
-
-    assert_match /rubyadmin_optout@gmail.com/, admins_all.last
-    assert admins_talk_true.empty?
-    assert admins.empty?
   end
 
 end
