1eba9d2a6f406bce740ecfeb2df4711bb4a07b0e
Todd Lynam <TLynam@gmail.com>
2014-05-27 19:30:28 -0700

    Started member tests

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -1,10 +1,16 @@
 require "minitest_helper"
 
 class MembersControllerTest < MiniTest::Rails::ActionController::TestCase
+  def setup
+    @member = Member.create!(:name        => "Test Name",
+                             :verified    => true)
+  end
+
   def test_index
     get :index
     assert_response :success
-    assert_equal("Members", assigns(:title))
+    assert_equal("Members",           assigns(:title))
+    assert_match @member.name,        @response.body
   end
 
   def test_new
@@ -17,9 +23,16 @@ class MembersControllerTest < MiniTest::Rails::ActionController::TestCase
   end
 
   def test_create
+    @testmember = Member.create!(:name        => "Test Name",
+                                 :verified    => true)
+
     assert_difference('Member.count', 1) do
       post :create, member: {:name => "test", :email => "test@test.com", :ruby_gems_id => "qrush"}
     end
+    
     assert_redirected_to members_path
+    get :index
+    assert_response :success
+    assert_match @member.name,        @response.body
   end
 end
