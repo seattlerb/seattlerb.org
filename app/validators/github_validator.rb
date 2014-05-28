730d5e929fc86ec9f88795baaeec4a02931cfa53
Todd Lynam <TLynam@gmail.com>
2014-05-27 19:44:15 -0700

    Updated Github Validator

--- app/validators/github_validator.rb
+++ app/validators/github_validator.rb
@@ -1,6 +1,6 @@
 class GithubValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if HTTParty.get("https://api.github.com/users/#{value}", :headers => {"User-Agent" => "seattle.rb"})["html_url"] == nil
+    if HTTParty.get("https://api.github.com/users/#{value}", :headers => {"User-Agent" => "seattle.rb"}).headers["status"] == "404 Not Found"
       record.errors[attribute] << (options[:message] || "username not found")
     end
   end
