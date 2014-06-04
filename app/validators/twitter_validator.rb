ec2c9f72d18a8c5bec99df5c1b9815b563483ee9
Todd Lynam <TLynam@gmail.com>
2014-06-03 19:11:00 -0700

    Updated URL validator to be more consise.  Twitter validator should pass Travis

--- app/validators/twitter_validator.rb
+++ app/validators/twitter_validator.rb
@@ -1,14 +1,6 @@
 class TwitterValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-    end
-    begin
-      client.user(value)
-    rescue Twitter::Error::NotFound
+      if HTTParty.get("https://www.twitter.com/#{value}").code == 404 then
       record.errors[attribute] << (options[:message] || "user isn't found" )
     end
   end
--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,12 +1,7 @@
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    valid = begin
-      URI.parse(value).kind_of?(URI::HTTP)
-    rescue URI::InvalidURIError
-      false
-    end
-    unless valid
+    unless URI.parse(value).kind_of?(URI::HTTP)
       record.errors[attribute] << (options[:message] || "is an invalid URL")
-    end
+    end   
   end
 end
