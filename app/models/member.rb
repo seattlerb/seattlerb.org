8e2fd1c470573753ecd23cc1b973c4530af1299c
Ryan Davis <ryand-ruby@zenspider.com>
2014-09-14 03:25:19 -0700

    Some quick refactorings to make it a bit easier to read.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,6 @@
 class Member < ActiveRecord::Base
+  NONE = "missing_image.png"
+
   attr_accessor :username # fake attribute for spam trapping
   validates_length_of :username, :maximum => 0
 
@@ -38,20 +40,21 @@ class Member < ActiveRecord::Base
   end
 
   def missing_image?
-    image_url.blank? || image_url == "missing_image.png" || HTTParty.get(self.image_url).code != 200
+    image_url.blank? || image_url == NONE || HTTParty.get(image_url).code != 200
   end
 
   def get_twitter_image_url
-    begin
-      client = Twitter::REST::Client.new do |config|
-        config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-        config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-        config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-      end
-      return client.user(self.twitter).profile_image_uri_https(:bigger).to_s
-    rescue Twitter::Error::NotFound
-      return "missing_image.png"
+    return twitter_client.user(self.twitter).profile_image_uri_https(:bigger).to_s
+  rescue Twitter::Error::NotFound
+    return NONE
+  end
+
+  def twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
   end
 end
