0175a82d1c944b0170fb818ad2c2249b64ba1eb2
Todd Lynam <TLynam@gmail.com>
2015-04-10 15:57:29 -0700

    Remove completed talk migration update all to pass tests

--- db/migrate/20130110042952_add_completed_to_talks.rb
+++ db/migrate/20130110042952_add_completed_to_talks.rb
@@ -1,10 +1,5 @@
 class AddCompletedToTalks < ActiveRecord::Migration
   def change
     add_column :talks, :completed, :boolean, :default => false
-
-    Talk.all.each do |t|
-      t.completed = false
-      t.save
-    end
   end
 end
