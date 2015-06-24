51faa0fccd16ce8f523d74655d1c891141e1d7c1
Todd Lynam <TLynam@gmail.com>
2015-06-23 20:55:30 -0700

    Devise sign up redirect to members

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -54,9 +54,9 @@ class Members::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    members_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
