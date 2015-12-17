a5f8ae51164a91343dc3119a130fba3c114a590d
frictitious <frictitious@users.noreply.github.com>
2015-12-16 21:10:43 -0800

    changed rubygems.org url to use https

--- app/validators/rubygems_validator.rb
+++ app/validators/rubygems_validator.rb
@@ -3,7 +3,7 @@ require 'uri'
 
 class RubygemsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    uri = URI("http://rubygems.org/api/v1/owners/#{value}/gems.json")
+    uri = URI("https://rubygems.org/api/v1/owners/#{value}/gems.json")
 
     unless Net::HTTPSuccess === Net::HTTP.get_response(uri) then
       record.errors[attribute] << (options[:message] || "user isn't found" )
