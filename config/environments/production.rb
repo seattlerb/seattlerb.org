ca600d3fc0937dc7a3bc550eba50f00675ed2e28
Ryan Davis <ryand-ruby@zenspider.com>
2016-01-09 15:08:38 -0800

    remove deprecation for logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,7 @@ SeattlerbOrg::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
