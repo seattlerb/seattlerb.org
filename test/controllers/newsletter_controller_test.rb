64cfa3bb7804890622ac8e9f128195482bceaf65
Ryan Davis <ryand-ruby@zenspider.com>
2021-05-02 03:00:47 -0700

    Fixed mocking of parameters since you can't compare equality of hash & params now

--- test/controllers/newsletter_controller_test.rb
+++ test/controllers/newsletter_controller_test.rb
@@ -3,26 +3,28 @@ require "minitest_helper"
 class NewsletterControllerTest < ActionController::TestCase
   def test_subscribe_to_discussion
     mock = MiniTest::Mock.new
-    mock.expect :call, nil, [{'email' => 'a@email.com'}]
+    params = ActionController::Parameters.new('email' => 'a@email.com').permit!
+    mock.expect :call, nil, [params]
 
     ZenspiderSubscriber.stub :subscribe_to_discussion, mock do
       post :subscribe, params: { :subscribe => {:person => {:email => 'a@email.com'}, :subscribe_to_discussion => "1"} }
     end
 
-    mock.verify
+    assert_mock mock
 
     assert_redirected_to join_us_path
   end
 
   def test_subscribe_to_jobs
     mock = MiniTest::Mock.new
-    mock.expect :call, nil, [{'email' => 'a@email.com'}]
+    params = ActionController::Parameters.new('email' => 'a@email.com').permit!
+    mock.expect :call, nil, [params]
 
     ZenspiderSubscriber.stub :subscribe_to_jobs, mock do
       post :subscribe, params: { :subscribe => {:person => {:email => 'a@email.com'}, :subscribe_to_jobs => "1"} }
     end
 
-    mock.verify
+    assert_mock mock
 
     assert_redirected_to join_us_path
   end
