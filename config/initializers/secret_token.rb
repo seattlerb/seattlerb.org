9c3b5ffaa0bf4dc52163f9fa7a795e2270a1b2d7
Ryan Davis <ryand-ruby@zenspider.com>
2022-04-19 20:55:14 -0700

    Removed secret_token, we already have secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,15 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SeattlerbOrg::Application.config.secret_token = if Rails.env.production?
-  ENV.fetch("SECRET_TOKEN")
-else
-  ENV.fetch("SECRET_TOKEN", "81f3f7ae6f5e7ab84987dc9d6f7297e49fea643de945e92985a9eefc27502c25564cf6de29e5c160eaba8df4bbc27a5227ac2a90a16000e04461105560f6fdf6")
-end
-
 SeattlerbOrg::Application.config.secret_key_base = if Rails.env.production?
   ENV.fetch("SECRET_KEY_BASE")
 else
