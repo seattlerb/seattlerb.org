dbcb3800b7e011cce3eb07c54367a59239423e50
Ryan Davis <ryand-ruby@zenspider.com>
2022-04-19 21:37:03 -0700

    Bump rails_admin, added sassc-rails
    This removes haml, jquery, and other sub-deps
    
    This adds turbo-rails.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 RailsAdmin.config do |config|
+  config.asset_source = :sprockets
   config.current_user_method { current_admin }
 
   config.main_app_name = ['Seattle.rb', 'Admin']
