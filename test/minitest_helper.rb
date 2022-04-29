e38e2f0124e0dba7bf1e3d44f3b92124f853c4e7
Ryan Davis <ryand-ruby@zenspider.com>
2022-04-29 14:26:03 -0700

    Minor cleanup in minitest_helper.rb

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -6,22 +6,9 @@ require "rails/test_help"
 require "minitest/rails"
 require "minitest/pride"
 
-# Uncomment if you want Capybara in accceptance/integration tests
-# require "minitest/rails/capybara"
-
-# Uncomment if you want awesome colorful output
-# require "minitest/pride"
-
 class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
 end
 
-# Do you want all existing Rails tests to use MiniTest::Rails?
-# Comment out the following and either:
-# A) Change the require on the existing tests to `require "minitest_helper"`
-# B) Require this file's code in test_helper.rb
-
-# MiniTest::Rails.override_testunit!
 module FakeNetwork
   def good
     obj = Object.new
