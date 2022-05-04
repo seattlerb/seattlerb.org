38e1d4a2d65fbc5decc13bfd40a1dfcc79bef983
Ryan Davis <ryand-ruby@zenspider.com>
2022-05-04 10:38:30 -0700

    Disable active_storage entirely

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+require "active_record/railtie"
+# NO: require "active_storage/engine"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+# NO?: require "action_cable/engine"
+# NO?: require "action_mailbox/engine"
+require "action_text/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
