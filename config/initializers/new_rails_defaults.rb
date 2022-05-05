fbb22a24e9348325de9db67e54eda7d5ba5bfe8c
Ryan Davis <ryand-ruby@zenspider.com>
2022-05-05 10:47:18 -0700

    Remove legacy default initializers

--- config/initializers/new_rails_defaults.rb
+++
@@ -1,4 +0,0 @@
-ActiveRecord::Base.include_root_in_json     = true
-ActiveRecord::Base.store_full_sti_class     = true
-ActiveSupport.escape_html_entities_in_json  = false
-ActiveSupport.use_standard_json_time_format = true
