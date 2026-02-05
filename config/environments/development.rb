9c05471304b2bf3ea9bba864bc27db5e8cabe679
Ryan Davis <ryand-ruby@zenspider.com>
2026-02-04 16:02:56 -0800

    Switched to 1 db for queue and app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+  config.active_job.queue_adapter = :solid_queue
 
   # Highlight code that triggered redirect in logs.
   config.action_dispatch.verbose_redirect_logs = true
