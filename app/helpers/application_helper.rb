94e21cb31cca644481024485cabcd39edcfe8b0e
Ryan Davis <ryand-ruby@zenspider.com>
2026-04-14 20:11:09 -0700

    Make nav_item_link convert the spaces to nbsp. Should also fix log in and log out.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def nav_item_link link_label, link_path, **opts
     opts["aria-current"] = ("page" if current_page? link_path)
-    content_tag :li, link_to(link_label, link_path, **opts)
+    content_tag :li, link_to(link_label.gsub(/ /, "&nbsp;").html_safe, link_path, **opts)
   end
 end
