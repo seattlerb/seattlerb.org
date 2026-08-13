9dceff749e380531b174bb8deadb17b25fe651a0
Ryan Davis <ryand-ruby@zenspider.com>
2026-08-13 12:46:43 -0700

    Remove --ensure-latest from everywhere
    I missed one in bin/brakeman, which is rude!

--- config/ci.rb
+++ config/ci.rb
@@ -5,7 +5,7 @@ CI.run do
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
-  step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error --ensure-latest 15"
+  step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
   step "Tests: Rails", "bin/rails test"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
